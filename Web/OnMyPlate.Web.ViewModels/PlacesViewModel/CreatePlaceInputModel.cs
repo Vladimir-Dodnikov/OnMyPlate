5b6f9894e386ae9e836c173264d63d6f88f3886f
Vladimir_Dodnikov <Vladimir_Dodnikov@students.softuni.bg>
2020-12-13 22:42:48 +0200

    Implement Create Place MVC.

---
+++ Web/OnMyPlate.Web.ViewModels/PlacesViewModel/CreatePlaceInputModel.cs
@@ -0,0 +1,50 @@
+﻿namespace OnMyPlate.Web.ViewModels.PlacesViewModel
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
+
+    using Microsoft.AspNetCore.Http;
+    using OnMyPlate.Common;
+    using OnMyPlate.Data.Models.Places;
+
+    public class CreatePlaceInputModel : Image
+    {
+        [Required]
+        [StringLength(GlobalConstants.PlaceNameMaxLength, ErrorMessage ="The Name of the Place should be between 2 and 100 charactes.", MinimumLength =2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(GlobalConstants.PlaceDescriptionMaxLength, ErrorMessage = "The Information of the Place should be between 10 and 4000 charactes.", MinimumLength =10)]
+        [Display(Name ="Fill-in information about your place")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name ="Choose your logo:")]
+        public IFormFile LogoImage { get; set; }
+
+        [Required]
+        [Display(Name = "Enter original web address")]
+        public string WebUrl { get; set; }
+
+        [Required]
+        [StringLength(GlobalConstants.LocationMaxLength, ErrorMessage = "Coordinates shoul be between 5 and 10 integers! example: Latitude: 45.225, Longtitude: 22.548", MinimumLength = 5)]
+        public Location Location { get; set; }
+
+        [Required]
+        public Address Address { get; set; }
+
+        [Required]
+        public WorkTime WorkTime { get; set; }
+
+        public virtual ICollection<IFormFile> Images { get; set; }
+
+        public virtual IEnumerable<KeyValuePair<int, string>> Amentities { get; set; }
+
+        public virtual IEnumerable<KeyValuePair<int, string>> Cuisines { get; set; }
+
+        public virtual IEnumerable<KeyValuePair<int, string>> PaymentTypes { get; set; }
+
+        public virtual IEnumerable<KeyValuePair<int, string>> MusicTypes { get; set; }
+    }
+}
---
+++ Web/OnMyPlate.Web/Controllers/PlacesController.cs
@@ -0,0 +1,53 @@
+﻿namespace OnMyPlate.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using OnMyPlate.Services.Data;
+    using OnMyPlate.Web.ViewModels.PlacesViewModel;
+
+    public class PlacesController : Controller
+    {
+        private readonly IAmentityService amentityService;
+        private readonly ICuisineService cuisineService;
+        private readonly IPaymentService paymentsService;
+        private readonly IMusicService musicService;
+
+        public PlacesController(
+            IAmentityService amentityService,
+            ICuisineService cuisineService,
+            IPaymentService paymentsService,
+            IMusicService musicService)
+        {
+            this.amentityService = amentityService;
+            this.cuisineService = cuisineService;
+            this.paymentsService = paymentsService;
+            this.musicService = musicService;
+            this.paymentsService = paymentsService;
+        }
+
+        public IActionResult Create()
+        {
+            var viewModel = new CreatePlaceInputModel();
+            viewModel.Amentities = this.amentityService.GetAllAmentitiesAsKeyValuePairs();
+            viewModel.Cuisines = this.cuisineService.GetAllCuisineAsKeyValuePairs();
+            viewModel.PaymentTypes = this.paymentsService.GetAllPaymentTypesAsKeyValuePairs();
+            viewModel.MusicTypes = this.musicService.GetAllMusicTypesAsKeyValuePairs();
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreatePlaceInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                input.Amentities = this.amentityService.GetAllAmentitiesAsKeyValuePairs();
+                input.Cuisines = this.cuisineService.GetAllCuisineAsKeyValuePairs();
+                input.PaymentTypes = this.paymentsService.GetAllPaymentTypesAsKeyValuePairs();
+                input.MusicTypes = this.musicService.GetAllMusicTypesAsKeyValuePairs();
+                return this.View(input);
+            }
+
+            // TODO: Redirect to PlacePage
+            return this.Redirect("/");
+        }
+    }
+}
